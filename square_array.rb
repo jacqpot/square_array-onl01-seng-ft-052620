00bb13fd18f2a7bf4ec1695b9c7f639fb0686c40
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-28 13:25:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   new_numbers = []
-  array.each do |num|
-    num2 = num**2
-    new_numbers.push(num2)
+  array.each do |num|**2
+    new_numbers.push(num)
     [new_numbers]
   end
 end
